81abf7190eb47aaf23188790db63c87ed3ab8156
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-20 11:35:03 -0700

    added password svgs

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -52,6 +52,8 @@ const SignUpForm = () => {
             <div key={ind}>{error}</div>
           ))}
         </div>
+        <div className='form-input-container'>
+
           <input
             type='text'
             name='username'
@@ -59,32 +61,41 @@ const SignUpForm = () => {
             onChange={updateUsername}
             value={username}
             className='form-input'
-          ></input>
-          <input
-            type='text'
-            name='email'
-            placeholder='Email'
-            onChange={updateEmail}
-            value={email}
-            className='form-input'
-          ></input>
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            onChange={updatePassword}
-            value={password}
-            className='form-input'
-          ></input>
-          <input
-            type='password'
-            name='repeat_password'
-            placeholder='Repeat Password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            className='form-input'
-            required={true}
-          ></input>
+          />
+        </div>
+          <div className='form-input-container'>
+            <input
+              type='text'
+              name='email'
+              placeholder='Email'
+              onChange={updateEmail}
+              value={email}
+              className='form-input'
+            />
+          </div>
+          <div className='form-input-container'>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"viewBox="0 0 24 24" style={{fill: '#ffffff'}}><path d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9069372 8 4 8.9069372 4 10 L 4 20 C 4 21.093063 4.9069372 22 6 22 L 18 22 C 19.093063 22 20 21.093063 20 20 L 20 10 C 20 8.9069372 19.093063 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 6 10 L 18 10 L 18 20 L 6 20 L 6 10 z M 12 13 C 10.9 13 10 13.9 10 15 C 10 16.1 10.9 17 12 17 C 13.1 17 14 16.1 14 15 C 14 13.9 13.1 13 12 13 z"></path></svg>
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              onChange={updatePassword}
+              value={password}
+              className='form-input'
+            />
+          </div>
+          <div className='form-input-container'>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"viewBox="0 0 24 24" style={{fill: '#ffffff'}}><path d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9069372 8 4 8.9069372 4 10 L 4 20 C 4 21.093063 4.9069372 22 6 22 L 18 22 C 19.093063 22 20 21.093063 20 20 L 20 10 C 20 8.9069372 19.093063 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 6 10 L 18 10 L 18 20 L 6 20 L 6 10 z M 12 13 C 10.9 13 10 13.9 10 15 C 10 16.1 10.9 17 12 17 C 13.1 17 14 16.1 14 15 C 14 13.9 13.1 13 12 13 z"></path></svg>
+            <input
+              type='password'
+              name='repeat_password'
+              placeholder='Repeat Password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              className='form-input'
+              required={true}
+            />
+          </div>
         <button type='submit' className='form-login-btn'>Sign Up</button>
         <div className='form-toggle'>
             New here?&nbsp;

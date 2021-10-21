d44bab98b4cc80f022ba3e62ec4d56edf6fe279a
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-21 11:04:07 -0700

    restructed files, created exercise page

--- react-app/src/components/Dashboard/Dashboard.js
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { NavLink, Route, useParams, useRouteMatch } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
 import './Dashboard.css';
-import './Homepage.css';
+import ExercisePage from './ExercisePage/ExercisePage';
+import Homepage from './Homepage/Homepage';
 
 const Dashboard = () => {
   const { userId } = useParams();
   const { path, url } = useRouteMatch();
-  const { username } = useSelector(state => state.session.user);
 
   return (
     <div className='dashboard-container'>
@@ -31,13 +31,10 @@ const Dashboard = () => {
       </div>
       <div className='dashboard-content-container'>
         <Route path={`${path}`} exact>
-          <div className='homepage-container'>
-           <h1 className='homepage-heading'>Welcome back</h1>
-           <h2 className='homepage-subheading'>{ username }</h2>
-          </div>
+          <Homepage />
         </Route>
         <Route path={`${path}/exercise`} >
-          exercise
+          <ExercisePage />
         </Route>
         <Route path={`${path}/diet`} >
           diet
---
+++ react-app/src/components/Dashboard/ExercisePage/ExercisePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExercisePage = () => {
+  return (
+    'hi'
+  )
+}
+
+export default ExercisePage;  

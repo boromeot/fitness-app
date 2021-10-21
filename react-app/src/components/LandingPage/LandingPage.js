db148b9fa8b7c7cf5b1e7f9e4ec603e5914e6cd0
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-21 08:42:59 -0700

    added home page logo, more home page styling

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import './about-links.css';
 const LandingPage = () => {
   return (
     <div className='landing-page-container'>
-      <div>
+        <img className='landing-page-logo' src='https://i.imgur.com/awqVsCJ.png' alt='' />
         <div className='about-links-container'>
           <a href='https://github.com/boromeot' target='_blank' rel="noreferrer" className='about-links'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
@@ -27,6 +27,9 @@ const LandingPage = () => {
           <h2 className='landing-page-card-heading'>
             Achieve your goals
           </h2>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
         </div>
         <div className='landing-page-card'>
           <h2 className='landing-page-card-heading'>
@@ -37,14 +40,23 @@ const LandingPage = () => {
         </div>
         <div className='landing-page-card'>
           <h2 className='landing-page-card-heading'>
-            Achieve your goals
+            Track your progress
           </h2>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
+          <p className='landinge-page-card-info'>Lorem ipsum dorlor me mos vatan di supta</p>
         </div>
       </section>
-      <section className='landing-page-section' id='landing-page-insipring'>
-
+      <section className='landing-page-section' id='landing-page-inspiring'>
+        <div className='landing-page-inspiring-spacer'>
+          <div className='landing-page-inspiring-text'>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <div className='landing-page-inspiring-carousel'>
+            IMAGE
+          </div>
+        </div>
       </section>
-      </div>
     </div>
 
   )

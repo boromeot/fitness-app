b552a65d50ffd3d2791a740ea194e9fe0e8cedd6
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-21 10:21:28 -0700

    created left panel component and links

--- react-app/src/components/Dashboard/Dashboard.js
+++ react-app/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Dashboard.css';
 
 const Dashboard = () => {
   return (
-    'hi'
+    <div>
+      <div className='dashboard-panel'>
+        <div className='dashboard-logo-container'>
+          <img className='dashboard-logo' src='https://i.imgur.com/awqVsCJ.png' alt='' />
+        </div>
+        <div className='dashboard-link-container'>
+          <NavLink to='/users/1/dashboard/exercise' className='dashboard-link' activeClassName='active'>
+            Exercise
+          </NavLink>
+          <NavLink to='/users/1/dashboard/ggg' className='dashboard-link'>
+            Diet
+          </NavLink>
+          <NavLink to='/users/1/dashboard/hhh' className='dashboard-link'>
+            Progress
+          </NavLink>
+        </div>
+      </div>
+    </div>
   )
 };
 

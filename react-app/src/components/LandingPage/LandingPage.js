954c76d9b65d93d5299a1f4de6a8cc920ce3d4d9
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-20 17:47:10 -0700

    added about links

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './LandingPage.css';
+import './about-links.css';
 
 const LandingPage = () => {
   return (
     <div className='landing-page-container'>
+      <div>
+        <div className='about-links-container'>
+          <a href='https://github.com/boromeot' target='_blank' rel="noreferrer" className='about-links'>
+            Github
+          </a>
+          <a href='https://www.linkedin.com/in/kekoa-boromeo/' target='_blank' rel="noreferrer" className='about-links'>
+            Linkedin
+          </a>
+        </div>
       <section className='landing-page-section' id='landing-page-header'>
         <div className='landing-page-heading-container'>
           <h1 className='landing-page-heading'>TITLE</h1>
@@ -15,6 +25,7 @@ const LandingPage = () => {
       <section className='landing-page-section'>
 
       </section>
+      </div>
     </div>
 
   )

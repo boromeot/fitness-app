f4d64b726cceb276d97331f2b3b85deea3b276ea
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-21 20:27:40 -0700

    Created edit delete buttons

--- react-app/src/components/Dashboard/ExercisePage/CyclesPage/CyclesPage.js
+++ react-app/src/components/Dashboard/ExercisePage/CyclesPage/CyclesPage.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCycles, postCycle } from "../../../../store/cycles";
+import { postCycle } from "../../../../store/cycles";
 import './CyclesPage.css';
 import '../../../../stylesheets/buttons.css';
 
@@ -8,23 +8,47 @@ const CyclesPage = () => {
   const dispatch = useDispatch();
   const { id:userId } = useSelector(state => state.session.user);
   const { cycles } = useSelector(state => state);
+  const [showEdit, setShowEdit] = useState(false);
 
-  const handleClick = e => {
+  const toggleEdit = e => {
+    e.preventDefault();
+    setShowEdit(prev => !prev);
+  }
+
+  const handleCreate = e => {
     e.preventDefault();
     dispatch(postCycle('test', userId))
+  }
+
+  const handleDelete = e => {
+    e.preventDefault();
+    
   }
 
   return (
     <div className='cycles-page-container'>
       <div className='cycles-page-button-container'>
-        <button className='cycles-page-create primary-btn btn' onClick={handleClick}>
+        <button className='cycles-page-edit edit-btn btn' onClick={toggleEdit}>
+          Edit mode
+        </button>
+        <button className='cycles-page-create primary-btn btn' onClick={handleCreate}>
           Create new cycle
         </button>
       </div>
-      <div>
+      <div className='cycle-conatiner'>
         {
           cycles?.map(cycle => {
-            return <div>{cycle.name}</div>
+            return (
+              <div className='cycle'>
+                {cycle.name}
+                {showEdit &&
+                  <>
+                    <button className='edit-btn btn'>Edit</button>
+                    <button className='delete-btn btn'>Delete</button>
+                  </>
+                }
+              </div>
+            )
           })
         }
       </div>

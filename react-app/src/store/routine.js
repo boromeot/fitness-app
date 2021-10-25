4497aa2ead44d1caa320f274f04234751d79c7b7
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-25 10:25:39 -0700

    created postROutine thunk

--- react-app/src/store/routine.js
+++ react-app/src/store/routine.js
@@ -15,6 +15,28 @@ const post_routine = routine => {
   };
 }
 
+export const postRoutine = (name, userId) => async dispatch => {
+  const response = await fetch(`/api/routines/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      user_id: userId
+    })
+  });
+  if (response.ok) {
+    const routine = await response.json();
+    dispatch(post_routine(routine));
+    return routine;
+  }
+  else {
+    const data = await response.json();
+    return data;
+  }
+}
+
 //Get routines by cycle id
 export const getRoutines = (cycleId) => async dispatch => {
 

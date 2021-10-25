7a663390cb83eb993bcff3b361f3e8a038062aee
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-25 15:11:04 -0700

    refactored cyclesPage to remove template. Templates just over complicated everything

--- react-app/src/components/Dashboard/ExercisePage/CyclesPage/CyclesPage.js
+++ react-app/src/components/Dashboard/ExercisePage/CyclesPage/CyclesPage.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import PageTemplate from "../../templates/PageTemplate";
 import CycleForm from "./CycleForm";
 import { deleteCycle } from "../../../../store/cycles";
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import Card from "../../templates/Card";
+import Modal from "../../../Modal/Modal";
 
 const CyclesPage = () => {
   const { cycles } = useSelector(state => state);
   const { url } = useRouteMatch();
   const [showEditButtons, setShowEditButtons] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleEditMode = e => {
+    e.preventDefault();
+    setShowEditButtons(prev => !prev);
+  }
+
+  const handleCreate = e => {
+    e.preventDefault();
+    setShowModal(true);
+  }
 
   return (
-    <PageTemplate name='cycle' Form={CycleForm} setShowEditButtons={setShowEditButtons}>
+    <div className='page-template-container'>
+    <div className='page-template-button-container'>
+      <button className='page-template-edit edit-btn btn' onClick={toggleEditMode}>
+        Edit mode
+      </button>
+      <button className='page-template-create primary-btn btn' onClick={handleCreate}>
+        {`Create new Cycle`}
+      </button>
+    </div>
+    <div className='card-conatiner'>
       {
         cycles?.map(cycle => {
           return (
@@ -22,7 +42,11 @@ const CyclesPage = () => {
           )
         })
       }
-    </PageTemplate>
+    </div>
+    <Modal title={`Create a Cycle`} onClose={() => setShowModal(false)} show={showModal}>
+      <CycleForm setShowModal={setShowModal} method='POST' component='Cycle'/>
+    </Modal>
+  </div>
   )
 }
 

f1e832c359a74b2972ad4c965d92e23a18ff430a
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-25 20:55:45 -0700

    added days of the week

--- react-app/src/components/Dashboard/ExercisePage/RoutinePage/RoutinePage.js
+++ react-app/src/components/Dashboard/ExercisePage/RoutinePage/RoutinePage.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
 import Modal from "../../../Modal/Modal";
 import RoutineForm from "../RoutinesPage/RoutineForm";
+import './RoutinePage.css';
 
 const RoutinePage = () => {
   const { userId, cycleId, routineId } = useParams();
   const [showCreateModal, setShowCreateModal] = useState(false);
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   const handleCreate = e => {
     e.preventDefault();
@@ -23,6 +25,13 @@ const RoutinePage = () => {
         <NavLink to={`/users/${userId}/dashboard/exercise/cycles/${cycleId}/routines`} className='back-btn btn'>
           Back
         </NavLink>
+      </div>
+      <div className='routine-page-days-container'>
+        {
+          days.map((day, i) => {
+            return <div className='routine-page-day' key={i}>{day}</div>
+          })
+        }
       </div>
       <Modal title={`Create a Routine`} onClose={() => setShowCreateModal(false)} show={showCreateModal}>
         <RoutineForm setShowModal={setShowCreateModal} method='POST' cycleId={cycleId}/>

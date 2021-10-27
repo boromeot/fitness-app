9e76f31ef6442e32739444b1ba1c8f2089315f96
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-27 01:04:07 -0700

    created exerciseform

---
+++ app/forms/exercise_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
+
+class ExerciseForm(FlaskForm):
+  name = StringField('name', validators=[DataRequired('Name required')])
+  body_part = StringField('name', validators=[DataRequired('Body part name required')])
+  user_id = IntegerField('user_id', validators=[DataRequired()])
+  work_id = IntegerField('user_id', validators=[DataRequired()])

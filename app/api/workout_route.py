8e534c210dba9a4f24a123a2413144d66336f288
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-26 19:12:39 -0700

    created workout post route

--- app/api/workout_route.py
+++ app/api/workout_route.py
@@ -1,21 +1,34 @@
 from flask import Blueprint, jsonify, request
 from app.api.auth_routes import login
+from app.forms.workout_form import WorkoutForm
 from flask_login import login_required, current_user
-from app.models import Exercise, db
+from app.models import Workout, db
 
 
 workout_routes = Blueprint('routines', __name__)
 
 def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{error}')
-    return errorMessages
+	"""
+	Simple function that turns the WTForms validation errors into a simple list
+	"""
+	errorMessages = []
+	for field in validation_errors:
+		for error in validation_errors[field]:
+			errorMessages.append(f'{error}')
+	return errorMessages
 
 @workout_routes.route('/', methods=['POST'])
 @login_required
 def post_exercise():
+	form = WorkoutForm()
+	form['csrf_token'].data = request.cookies['csrf_token']
+	if form.validate_on_submit():
+		workout = Workout(
+			day = form.data['day'],
+			user_id = form.data['user_id'],
+			routine_id = form.data['routine_id']
+		)
+		db.session.add(workout)
+		db.session.commit()
+		return workout.to_dict()
+	return {'errors': validation_errors_to_error_messages(form.errors)}, 401

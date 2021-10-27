769c50c4ad9c9a66cfd78233a91caaef26fae47b
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-27 01:08:32 -0700

    created exercise post route

--- app/api/exercise_routes.py
+++ app/api/exercise_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.api.auth_routes import login
+from app.forms.exercise_form import ExerciseForm
 from flask_login import login_required, current_user
 from app.models import Exercise, db
 
@@ -19,4 +20,15 @@ def validation_errors_to_error_messages(validation_errors):
 @login_required
 def post_exercise():
   form = ExerciseForm()
-  
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    exercise = Exercise(
+      name = form.data['name'],
+      body_part = form.data['body_part'],
+      user_id = form.data['user_id'],
+      work_id = form.data['work_id']
+    )
+    db.session.add(exercise)
+    db.session.commit()
+    return exercise.to_dict()
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401

d6cc5569c1f3b7ca73b73845efe7681ce615a18a
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-26 19:43:15 -0700

    added post_weekly_workout case to cycles reducer

--- react-app/src/store/cycles.js
+++ react-app/src/store/cycles.js
@@ -1,5 +1,5 @@
 import { POST_ROUTINE, DELETE_ROUTINE, PATCH_ROUTINE } from "./routine";
-import { POST_WORKOUT } from "./workout";
+import { POST_WORKOUT, POST_WEEKLY_WORKOUT } from "./workout";
 
 const GET_CYCLES = 'cycles/getCycles';
 const POST_CYCLE = 'cycles/postCycle';
@@ -154,6 +154,12 @@ export default function cycles(state = [], action) {
       i = newState.findIndex(cycle => cycle.id === action.payload.cycleId);
       j = newState[i].routines.findIndex(routine => routine.id === action.payload.routineId);
       newState[i].routines[j].workouts.push(action.payload.workout);
+      return newState;
+    case POST_WEEKLY_WORKOUT:
+      newState = [...state];
+      i = newState.findIndex(cycle => cycle.id === action.payload.cycleId);
+      j = newState[i].routines.findIndex(routine => routine.id === action.payload.routineId);
+      newState[i].routines[j].workouts = action.payload.workouts;
       return newState;
     default:
       return state;

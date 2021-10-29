bba0376d10314dc7c4c115401e15e5150f207a69
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-29 09:02:50 -0700

    basic hover effects on buttons

--- react-app/src/components/Dashboard/Navigation.js
+++ react-app/src/components/Dashboard/Navigation.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { NavLink, useRouteMatch } from "react-router-dom";
 import LogoutButton from '../auth/LogoutButton';
 import './Navigation.css';
 
 const Navigation = ({ children }) => {
   const { username } = useSelector(state => state.session.user);
-  const { url } = useRouteMatch();
   return (
     <nav className='navigation-container'>
       {/* <NavLink to={`${url}/exercise/cycles`} className='navigation-cycle'>
